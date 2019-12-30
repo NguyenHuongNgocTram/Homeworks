954c455ab9cb62d12e16a821dd3d34e308943b96
NguyenHuongNgocTram <54984058+NguyenHuongNgocTram@users.noreply.github.com>
2019-12-30 10:57:47 +0700

    Update Project2a.php

--- Project2a.php
+++ Project2a.php
@@ -16,13 +16,19 @@
 	<title></title>
 </head>
 <body>
-	<form action="Project2a.php" method="post">
-		<h1>Login</h1>
-		<label>UserName</label>
-		<input type="text" name="UserName">
-		<label>Password</label>
-		<input type="password" name="Passwords">
-		<button name="login">LOGIN</button>
+	<form action="Project1b.php" method="post" style=" background-color: lightblue; margin-left: 200px;margin-right: 300px;">
+		<div style="margin-left: 200px">
+			<h1 style="color: red;"> Login</h1>
+			<label style="font-size: 29px">UserName</label>
+			<br>
+			<input type="text" name="UserName" style="margin-top: 10px;">
+			<br>
+			<label style="font-size: 29px"style="margin-top: 10px;" >Password</label>
+			<br>
+			<input type="password" name="Passwords" style="margin-top: 10px;">
+			<br>
+			<button name="login" style="margin-top: 10px;">LOGIN</button>
+		</div>
 	</form>
 
 </body>
