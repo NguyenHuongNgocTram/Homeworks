6c683135e344f1175ac5f0b18115dd29b34f3073
NguyenHuongNgocTram <54984058+NguyenHuongNgocTram@users.noreply.github.com>
2019-12-30 11:43:56 +0700

    Delete Project3b.php

--- Project3b.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-if ($_SESSION["UserNames"] ==$resuilt[1][1] && $_SESSION["Passwords"] ==$resuilt[1][2] ) {
-			echo $_SESSION["UserName"];
-			echo $_SESSION["Passwords"];
-		} 
- ?>
